1765f5c650f337391df5416299bbf28ba201ed91
KASHIF MAJID <53035827+kashif-majid@users.noreply.github.com>
2020-09-08 17:03:11 +0530

    Add files via upload

---
+++ returns true if sorted otherwise false.py
@@ -0,0 +1,18 @@
+def sort(x):
+    if x==sorted(x):
+        return True
+    else:
+        print(False)
+        return sorted(a)
+
+i=1
+while i==1:
+    print("ENTER THREE INTEGERS:")
+    a=[]
+    for i in range(0,3):
+        a.append(int(input()))
+    print(sort(a))
+
+    i = int(input("\nIF U WANT TO CONTINUE PRESS 1 ELSE PRESS ANY KEY: "))
+
+
